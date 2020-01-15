aaf6b455dfab7d09960025e4bca5e7f394f53966
Armaan Roshani <armaan@gmail.com>
2020-01-14 20:57:37 -0800

    values convert to JSON now
    all values except a.Services. Uncertain how to convert from custom type UUID to something JSON understands.
     Please enter the commit message for your changes. Lines starting

--- main.go
+++ main.go
@@ -1,3 +1,9 @@
+// 
+// author: armaan roshani
+//
+// derived from: github.com/go-ble/ble/examples/basic/scanner
+//
+
 package main
 
 import (
@@ -6,10 +12,13 @@ import (
 	"fmt"
 	"log"
 	"time"
+	"encoding/json"
+	//"os"
 
 	"github.com/go-ble/ble"
 	"github.com/go-ble/ble/examples/lib/dev"
 	"github.com/pkg/errors"
+	//"github.com/eclipse/paho.mqtt.golang"
 )
 
 var (
@@ -17,6 +26,17 @@ var (
 	du     = flag.Duration("du", 5*time.Second, "scanning duration")
 	dup    = flag.Bool("dup", true, "allow duplicate reported")
 )
+
+// Struct to hold values to convert to JSON
+type BleDev struct {
+    Addr		string
+	Rssi		int64
+	Contable	bool
+    Name    	string
+	Services	[]string
+    ManData     []byte
+    Scanned 	time.Time
+}
 
 func main() {
 	flag.Parse()
@@ -27,6 +47,8 @@ func main() {
 	}
 	ble.SetDefaultDevice(d)
 
+
+
 	// Scan for specified durantion, or until interrupted by user.
 	fmt.Printf("Scanning for %s...\n", *du)
 	ctx := ble.WithSigHandler(context.WithTimeout(context.Background(), *du))
@@ -34,24 +56,41 @@ func main() {
 }
 
 func advHandler(a ble.Advertisement) {
+	bDev := BleDev{
+		Addr:		a.Addr().String(),
+		Rssi:		int64(a.RSSI()),
+		Contable:	false,
+	}
+
 	if a.Connectable() {
+		bDev.Contable = true
 		fmt.Printf("[%s] C %3d:", a.Addr(), a.RSSI())
 	} else {
 		fmt.Printf("[%s] N %3d:", a.Addr(), a.RSSI())
 	}
 	comma := ""
 	if len(a.LocalName()) > 0 {
+		bDev.Name = a.LocalName()
 		fmt.Printf(" Name: %s", a.LocalName())
 		comma = ","
 	}
 	if len(a.Services()) > 0 {
+		//bDev.Services = string(a.Services())
 		fmt.Printf("%s Svcs: %v", comma, a.Services())
 		comma = ","
 	}
 	if len(a.ManufacturerData()) > 0 {
+		bDev.ManData = a.ManufacturerData()
 		fmt.Printf("%s MD: %X", comma, a.ManufacturerData())
 	}
 	fmt.Printf("\n")
+
+	var jsonData []byte
+	jsonData, err := json.Marshal(bDev)
+	if err != nil {
+    	log.Println(err)
+	}
+	fmt.Println(string(jsonData))
 }
 
 func chkErr(err error) {

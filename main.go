f4ecf47e378905bfe3570bc68f1f680d832b1c74
Armaan Roshani <armaan@gmail.com>
2020-01-15 21:05:32 -0500

    better formatting

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-// 
+//
 // author: armaan roshani
 //
 // derived partially from: github.com/go-ble/ble/examples/basic/scanner
@@ -9,11 +9,11 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
 	"time"
-	"encoding/json"
 	//"os"
 
 	"github.com/go-ble/ble"
@@ -28,7 +28,6 @@ type advType struct {
 	name string
 }
 
-
 var (
 	// BLE
 	device = flag.String("device", "default", "implementation of ble")
@@ -36,31 +35,29 @@ var (
 	dup    = flag.Bool("dup", true, "allow duplicate reported")
 
 	// MQTT
-	topic  	  = flag.String("topic", "ble/test", "The topic name to publish to. (default ble/test)")
-	broker 	  = flag.String("broker", "tcp://127.0.0.1:1883", "The broker URI. ex: tcp://10.10.1.1:1883")
+	topic     = flag.String("topic", "ble/test", "The topic name to publish to. (default ble/test)")
+	broker    = flag.String("broker", "tcp://127.0.0.1:1883", "The broker URI. ex: tcp://10.10.1.1:1883")
 	password  = flag.String("password", "", "The password (optional)")
-	user 	  = flag.String("user", "", "The User (optional)")
-	id 		  = flag.String("id", "bleTest", "The ClientID (optional)")
+	user      = flag.String("user", "", "The User (optional)")
+	id        = flag.String("id", "bleTest", "The ClientID (optional)")
 	cleansess = flag.Bool("clean", false, "Set Clean Session (default false)")
-	qos 	  = flag.Int("qos", 0, "The Quality of Service 0,1,2 (default 0)")
-	store 	  = flag.String("store", ":memory:", "The Store Directory (default use memory store)")
+	qos       = flag.Int("qos", 0, "The Quality of Service 0,1,2 (default 0)")
+	store     = flag.String("store", ":memory:", "The Store Directory (default use memory store)")
 )
 
 // values to convert to JSON
 type BleDev struct {
-    Addr		string
-	Rssi		int64
-	Contable	bool
-    Name    	string
-	Services	[]string
-    ManData     []byte
-    Scanned 	time.Time
+	Addr     string
+	Rssi     int64
+	Contable bool
+	Name     string
+	Services []string
+	ManData  []byte
+	//Scanned 	time.Time
 }
 
 func main() {
 	flag.Parse()
-
-	//messages := make(chan string, 100)
 
 	// BLE setup
 	d, err := dev.NewDevice(*device)
@@ -85,39 +82,32 @@ func main() {
 		panic(token.Error())
 	}
 
-
 	myChan := make(chan string, 1000)
 	// create new object with channel
 	newAdv := &advType{
-		c:    myChan, 
-		name: "main advertiser",         // give this advertiser a name
+		c:    myChan,
+		name: "main advertiser", // give this advertiser a name
 	}
-
 
 	go bleScan(*newAdv)
 	mqttSend(client, *newAdv)
-
-
 }
-
 
 func bleScan(newAdv advType) {
 	// BLE Scan for specified duration, or until interrupted by user.
 	fmt.Printf("Scanning for %s...\n", *du)
 	ctx := ble.WithSigHandler(context.WithTimeout(context.Background(), *du))
-	chkErr(ble.Scan(ctx, *dup, newAdv.advHandler, nil))   // pass adv method to scan func
+	chkErr(ble.Scan(ctx, *dup, newAdv.advHandler, nil)) // pass adv method to scan func
 	defer close(newAdv.c)
 }
 
-
-
 // make advHandler method of adv type
 func (adv *advType) advHandler(a ble.Advertisement) {
-	
+
 	bDev := BleDev{
-		Addr:		a.Addr().String(),
-		Rssi:		int64(a.RSSI()),
-		Contable:	false,
+		Addr:     a.Addr().String(),
+		Rssi:     int64(a.RSSI()),
+		Contable: false,
 	}
 
 	if a.Connectable() {
@@ -136,14 +126,13 @@ func (adv *advType) advHandler(a ble.Advertisement) {
 	var jsonData []byte
 	jsonData, err := json.Marshal(bDev)
 	if err != nil {
-    	log.Println(err)
+		log.Println(err)
 	}
 	//fmt.Println(string(jsonData))
 	adv.c <- string(jsonData)
 }
 
-
-func mqttSend(client MQTT.Client,adv advType) {
+func mqttSend(client MQTT.Client, adv advType) {
 	var i int
 	for m := range adv.c {
 		client.Publish(*topic, byte(*qos), false, m)
@@ -152,7 +141,6 @@ func mqttSend(client MQTT.Client,adv advType) {
 	}
 	fmt.Println("Sent ", i, " MQTT messages")
 }
-
 
 func chkErr(err error) {
 	switch errors.Cause(err) {
